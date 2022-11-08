556d4bb4c73cc6a4419c0a94c7a1484503d0e976
David Santiago Gonzalez <ds.gon@outlook.com>
2022-11-07 21:58:28 -0300

    agrego estructura AAA en test

--- clase_05/webelements_test.py
+++ clase_05/webelements_test.py
@@ -23,6 +23,9 @@ driver.maximize_window()
 time.sleep(3)
 
 def test_login_laboratorio():
+        # Asset
+        username = 'David'
+        password = 'Password'
         logo = driver.find_element(By.XPATH,'//img[@alt="logo"]')
         assert logo.is_displayed() , 'El logo de QA Minds no esta visible.'
         opcion_laboratorio = driver.find_element(By.XPATH,'//nav[contains(@class,"navbar-menu")]//a[text()="LABORATORIO"]')
@@ -30,19 +33,24 @@ def test_login_laboratorio():
         opcion_laboratorio.click()
         driver.switch_to.window(driver.window_handles[1])
         time.sleep(5)
+
+        #Actions
         login_form = driver.find_element(By.CSS_SELECTOR,'.salesforceIdentityLoginForm2')
         assert login_form.is_displayed() , 'El formulario de Login no se encuentra visible'
         input_username = driver.find_element(By.ID,'48:2;a')
         assert input_username.is_displayed() , 'El campo Usuario no esta visible'
-        input_username.send_keys('David')
+        input_username.send_keys(username)
         input_password = driver.find_element(By.XPATH,'//input[@type="password"]')
         assert input_password.is_displayed() , 'El campo Password no esta visible'
-        input_password.send_keys('Password')
+        input_password.send_keys(password)
         button_login = driver.find_element(By.TAG_NAME,'button')
         assert button_login.is_displayed() , 'El boton de iniciar sesion no esta visible'
         assert button_login.is_enabled() , 'El button de inicio de sesion no se encuentra habilitado'
         button_login.click()
         time.sleep(3)
+
+        #Assert
+        assert not login_form.is_displayed() , 'El formulario de Login sigue visible, cuando no deberia'
         
 
 def teardown():
