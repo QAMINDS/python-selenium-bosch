ed103edf7a8f7023c78aec339ca770cb08d8e1f3
David Santiago Gonzalez <ds.gon@outlook.com>
2022-12-05 23:01:51 -0300

    clase 16: Construccion de Framework (cont)

--- clase_16_framework/pages/menu_page.py
+++ clase_16_framework/pages/menu_page.py
@@ -3,15 +3,32 @@ from selenium.webdriver.common.by import By
 
 class MenuPage(BasePage):
 
+    __button_search = {'by':By.CSS_SELECTOR, 'locator':'#search button'}
+    __input_buscar = {'by':By.NAME,'locator':'search'}
+    __img_logo = {'by':By.CSS_SELECTOR,'locator':'#logo img'}
+
     def __init__(self, driver) -> None:
         super().__init__(driver)
-
-    __boton_buscar = '#search button'
+        self.__verificar_menu()
+        
+    def __verificar_menu(self):
+        img_element = self._get_element(self.__img_logo['by'],self.__img_logo['locator'])
+        self._is_present(img_element,'No se encuentra la Imagen del Logo')
+        self._is_visible(img_element,'El Logo no se encuentra visible')
 
     def click_boton_buscar(self):
-        button_element = self._get_element(By.CSS_SELECTOR, self.__boton_buscar)
+        button_element = self._get_element(self.__button_search['by'], self.__button_search['locator'])
         self._is_present(button_element, 'No se encuentra el boton Buscar')
         self._is_visible(button_element, 'El boton buscar no se encuentra visible') 
         self._is_enabled(button_element, 'El boton de Busqueda no se encuentra habilitado') 
         self._click(button_element)
-        
+
+    def ingresar_busqueda(self,text):
+        input_element = self._get_element(self.__input_buscar['by'],self.__input_buscar['locator'])
+        self._is_present(input_element,'No se encuentra el input de Busqueda')
+        self._is_visible(input_element,'El input de Busqueda no se encuentra visible')
+        self._write(input_element,text)
+
+    def buscar_producto(self,text):
+        self.ingresar_busqueda(text)
+        self.click_boton_buscar()
--- clase_16_framework/tests/test_search.py
+++ clase_16_framework/tests/test_search.py
@@ -1,12 +1,21 @@
 import factories_drivers.factory_driver as f_driver
 from pages.menu_page import MenuPage
 from pages.base_page import BasePage
+from pages.search_page import SearchPage
+import time
 
 driver = f_driver.get_driver()
 
 def test_boton_buscar():
-    menu_page = MenuPage(driver)
-    menu_page.click_boton_buscar()
+    producto = 'Display'
+    busqueda_invalida_text = 'There is no product that matches the search criteria.'
+    menu_page = MenuPage(driver) 
+    menu_page.buscar_producto(producto)
+    search_page = SearchPage(driver)
+    search_page.verificar_no_producto(busqueda_invalida_text)
+    search_page.check_product_description()
+    search_page.click_search()
+    time.sleep(2)
 
 def teardown():
     BasePage(driver).close_browser()
